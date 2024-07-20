e5df5e8f7f8dddd7104b45fead421e0c0900acfb
Иван Павлович Шелдяков <i.sheldyakov@g.nsu.ru>
2024-07-20 18:59:05 +0700

    init confing server

---
+++ src/main/java/pet/project/TryingSpringCloud/TryingSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package pet.project.TryingSpringCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryingSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/project/TryingSpringCloud/TryingSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.TryingSpringCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryingSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
